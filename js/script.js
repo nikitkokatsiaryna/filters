8cc37cbd40d2ad52ae21ec0cecb58619bd628f56
nikitkokatsiaryna <katya.nikitko@yandex.ru>
2018-02-20 00:24:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var controls = document.querySelectorAll('.toggle-controls li');
+var photo = document.querySelector('.photo');
+
+for (var i = 0; i < controls.length; i++) {
+    controls[i].innerHTML = controls[i].dataset.filter;
+    clickControl(controls[i]);
+}
+
+function toggleFilter(control) {
+    for (var i = 0; i < controls.length; i++) {
+        controls[i].classList.remove('active');
+        photo.classList.remove(controls[i].dataset.filter);
+    }
+
+    control.classList.add('active');
+
+    if (photo) {
+        photo.classList.add(control.dataset.filter);
+    }
+}
+
+function clickControl(control) {
+    control.addEventListener('click', function() {
+        toggleFilter(control);
+    });
+}
+
+var defaultFilter = document.querySelector('li.oldie');
+toggleFilter(defaultFilter);
